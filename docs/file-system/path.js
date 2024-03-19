8f210642ee59b02709c3369a781d5595959c3aa3
Justin OConnell <itsjustinoconnell@gmail.com>
2024-03-19 14:38:41 +0000

    basics fs within node

--- docs/file-system/path.js
+++ docs/file-system/path.js
@@ -6,6 +6,7 @@ console.log("Directory", path.dirname(list));
 console.log("Basename", path.basename(list));
 console.log("Extension", path.extname(list));
 
+/* STATS FROM FILE */
 const fileStats = (file) => {
   console.log("hi");
   fs.stat(file, (err, stats) => {
@@ -18,3 +19,28 @@ const fileStats = (file) => {
 };
 
 fileStats(list);
+
+/* READ FILE */
+fs.readFile(list, (err, data) => {
+  if (err) console.log(err);
+
+  console.log("Buffer:", data);
+  console.log("Buffer to String:", data.toString());
+});
+
+/* WRITE TO FILE */
+
+let text = "See you in three months Trev";
+
+fs.writeFile(list, text, (err) => {
+  if (err) console.log(err);
+
+  console.log("File written");
+});
+
+/* APPEND TEXT TO A FILE */
+fs.appendFile(list, "Glad you are back to work", (err) => {
+  if (err) console.log(err);
+
+  console.log("content appended to file");
+});
